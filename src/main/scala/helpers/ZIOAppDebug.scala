e8a9a373236824e9962d070ed6baba5dfb337959
James Ward <james@jamesward.com>
2024-06-12 10:11:04 -0600

    maybe works this time #james #time 1h

---
+++ src/main/scala/helpers/ZIOAppDebug.scala
@@ -0,0 +1,26 @@
+package helpers
+
+import zio.*
+
+
+// Enables us to add a debug to print the output of the program
+trait ZIOAppDebug:
+  self =>
+
+  type Environment = Any
+
+  val bootstrap: ZLayer[ZIOAppArgs, Any, Any] = ZLayer.empty
+
+  val environmentTag: EnvironmentTag[Any] = EnvironmentTag[Any]
+
+  def run: ZIO[ZIOAppArgs & Scope, Any, Any]
+
+  def main(args: Array[String]): Unit =
+    val app = new ZIOAppDefault:
+      override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
+        self.bootstrap
+
+      override def run: ZIO[ZIOAppArgs & Scope, Any, Any] =
+        self.run.debug("Result")
+
+    app.main(args)
